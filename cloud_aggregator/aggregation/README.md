c4f6424c6591a9bfd84097f48100112489918600
Matt Iannucci <matt.iannucci@rpsgroup.com>
2023-08-10 14:38:17 -0400

    Add serverless aggregator with IAC, rename existing aggregation example to hrrr_aggregator

---
+++ cloud_aggregator/aggregation/README.md
@@ -0,0 +1,7 @@
+# Aggregation Service
+
+Service to generate zarr aggregations from kerchunked zarr metadata on s3
+
+This service uses the [ingest-tools](https://github.com/asascience-open/nextgen-dmac/blob/main/kerchunk/Dockerfile) base image which is published as `nextgen-dmac/ingest-tools` on ECR.
+
+## TODO: Everything here
---
+++ cloud_aggregator/ingest/ingest.py
@@ -0,0 +1,21 @@
+from ingest_tools.nos_ofs import generate_kerchunked_nos_sqs
+
+
+# TODO: Make these configurable
+DESTINATION_BUCKET_NAME='nextgen-dmac-cloud-ingest'
+DESTINATION_PREFIX='nos'
+FILTERS=['cbofs']
+
+
+def handler(event, context):
+    '''
+    This is the entry point for the ingest lambda function. It is responsible for
+    taking the event from the NOS new object topic and processing it. This means
+    scanning the netcdf file and extracting the metadata to create a virtual 
+    zarr representation of the dataset in the referenced object. 
+    '''
+    payload = event['Records'][0]['body']
+    
+    print(f'Ingesting SQS Message: {payload}')
+
+    generate_kerchunked_nos_sqs(payload, DESTINATION_BUCKET_NAME, DESTINATION_PREFIX, FILTERS)
