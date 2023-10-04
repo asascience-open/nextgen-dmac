f2a8b17b4a952981276851a4556a7c287c84107b
Matt Iannucci <matt.iannucci@rpsgroup.com>
2023-10-04 14:40:51 -0400

    More tests, add fvcom aggregation first pass

--- cloud_aggregator/aggregation/aggregation.py
+++ cloud_aggregator/aggregation/aggregation.py
@@ -1,32 +1,41 @@
-from ingest_tools.nos_ofs import generate_kerchunked_nos_roms_model_run, generate_kerchunked_nos_roms_best_time_series
+from ingest_tools.nos_ofs import (
+    generate_kerchunked_nos_roms_model_run,
+    generate_kerchunked_nos_roms_best_time_series,
+    generate_kerchunked_nos_fvcom_model_run,
+    generate_kerchunked_nos_fvcom_best_time_series,
+)
 from ingest_tools.rtofs import generate_kerchunked_rtofs_best_time_series
 from ingest_tools.aws import parse_s3_sqs_payload
 from ingest_tools.filters import key_contains
 
 
-NOS_ROMS_FILTERS = ['cbofs', 'ciofs', 'dbofs', 'tbofs', 'wcofs']
-RTOFS_FILTERS = ['rtofs']
+NOS_ROMS_FILTERS = ["cbofs", "ciofs", "dbofs", "tbofs", "wcofs"]
+NOS_FVCOM_FILTERS = ["ngofs2"]
+RTOFS_FILTERS = ["rtofs"]
 
 
 def handler(event, context):
-    '''
+    """
     This is the entry point for the aggregate lambda function. It is responsible for
-    taking the s3 event from the ingest bucket object updated notification 
-    and processing it. This means scanning the given key path, in the given bucket, 
-    finding all relevant virtual dataset file, aggragating them together into 
+    taking the s3 event from the ingest bucket object updated notification
+    and processing it. This means scanning the given key path, in the given bucket,
+    finding all relevant virtual dataset file, aggragating them together into
     a single virtual dataset, and writing that virtual dataset to the given bucket and
     key path.
-    '''
-    payload = event['Records'][0]['body']
+    """
+    payload = event["Records"][0]["body"]
 
-    print(f'Updating aggregations from given notification: {payload}')
+    print(f"Updating aggregations from given notification: {payload}")
 
     region, bucket, key = parse_s3_sqs_payload(payload)
 
     if key_contains(key, NOS_ROMS_FILTERS):
         generate_kerchunked_nos_roms_model_run(region, bucket, key)
         generate_kerchunked_nos_roms_best_time_series(region, bucket, key)
+    elif key_contains(key, NOS_FVCOM_FILTERS):
+        generate_kerchunked_nos_fvcom_model_run(region, bucket, key)
+        generate_kerchunked_nos_fvcom_best_time_series(region, bucket, key)
     elif key_contains(key, RTOFS_FILTERS):
         generate_kerchunked_rtofs_best_time_series(region, bucket, key)
     else:
-        print(f'No aggregation available for key: {key}')
+        print(f"No aggregation available for key: {key}")
--- ingest_tools/ingest_tools/nos_ofs.py
+++ ingest_tools/ingest_tools/nos_ofs.py
@@ -105,6 +105,48 @@ def generate_kerchunked_nos_roms_model_run(region: str, bucket: str, key: str):
     print(f'Successfully updated {outurl} NOS aggregation')
 
 
+def generate_kerchunked_nos_fvcom_model_run(region: str, bucket: str, key: str):
+    '''
+    Generate or update the multizarr kerchunked aggregation for the model run that the specified file belongs to
+    '''
+    try:
+        model_date, model_hour = parse_nos_model_run_datestamp(key)
+        model_run_glob = generate_nos_model_run_glob_expression(key, model_date, model_hour)
+    except Exception as e:
+        print(f'Failed to parse model run date and hour from key {key}: {e}. Skipping...')
+        return
+
+    # For now SSL false is solving my cert issues **shrug**
+    fs_read = fsspec.filesystem('s3', anon=True, skip_instance_cache=True, use_ssl=False)
+    fs_write = fsspec.filesystem('s3', anon=False, skip_instance_cache=True, use_ssl=False)
+
+    model_run_files = fs_read.glob(f's3://{bucket}/{model_run_glob}')
+    model_run_files = sorted(['s3://'+f for f in model_run_files])
+
+    model_run_file_count = len(model_run_files)
+    print(f'Aggregating {model_run_file_count} model files for model run {model_date} t{model_hour}z...')
+
+    # TODO: Generalize this somehow? 
+    mzz = MultiZarrToZarr(
+        model_run_files, 
+        remote_protocol='s3', 
+        remote_options={'anon': True, 'use_ssl': False},
+        concat_dims=['time'],
+        identical_dims=['lon', 'lat', 'lonc', 'latc', 'siglay', 'siglev', 'nele', 'node']
+    )
+
+    d = mzz.translate()
+
+    outkey = model_run_glob.replace('.f*', '')
+    outurl = f's3://{bucket}/{outkey}'
+
+    print(f'Writing zarr model aggregation to {outurl}')
+    with fs_write.open(outurl, 'w') as ofile:
+        ofile.write(ujson.dumps(d))
+    
+    print(f'Successfully updated {outurl} NOS aggregation')
+
+
 def generate_kerchunked_nos_roms_best_time_series(region: str, bucket: str, key: str):
     '''
     Generate or update the best time series kerchunked aggregation for the model run. If the specified file is not in the best time series, 
@@ -152,6 +194,67 @@ def generate_kerchunked_nos_roms_best_time_series(region: str, bucket: str, key:
         remote_options={'anon': True, 'use_ssl': False},
         concat_dims=['ocean_time'],
         identical_dims=['eta_rho', 'xi_rho', 's_rho', 'eta_psi', 'xi_psi', 's_w', 'eta_u', 'xi_u', 'eta_v', 'xi_v', 'lat_rho', 'lat_psi', 'lat_u', 'lat_v', 'lon_rho', 'lon_psi', 'lon_u', 'lon_v']
+    )
+
+    d = mzz.translate()
+
+    outkey = best_time_series_glob.replace('f*', 'best').replace('.*.t*z', '')
+    outurl = f's3://{bucket}/{outkey}'
+
+    print(f'Writing zarr best time series aggregation to {outurl}')
+    with fs_write.open(outurl, 'w') as ofile:
+        ofile.write(ujson.dumps(d))
+    
+    print(f'Successfully updated {outurl} NOS best time series aggregation')
+
+
+def generate_kerchunked_nos_fvcom_best_time_series(region: str, bucket: str, key: str):
+    '''
+    Generate or update the best time series kerchunked aggregation for the model run. If the specified file is not in the best time series, 
+    then the best time series aggregation will not be updated
+    '''
+    print(f'Generating best time series multizarr aggregation for key: {key}')
+
+    try:
+        best_time_series_glob = generate_nos_best_time_series_glob_expression(key)
+    except Exception as e: 
+        print(f'Failed to parse model run date and hour from key {key}: {e}. Skipping...')
+        return
+    
+    # For now SSL false is solving my cert issues **shrug**
+    fs_read = fsspec.filesystem('s3', anon=True, skip_instance_cache=True, use_ssl=False)
+    fs_write = fsspec.filesystem('s3', anon=False, skip_instance_cache=True, use_ssl=False)
+
+    model_files = fs_read.glob(f's3://{bucket}/{best_time_series_glob}')
+    model_files = sorted(['s3://'+f for f in model_files])
+
+    indexes = {}
+
+    for f in model_files:
+        model_date_key, offset = parse_nos_model_run_datestamp_offset(f)
+        if model_date_key not in indexes:
+            indexes[model_date_key] = [offset, f]
+        else: 
+            if offset < indexes[model_date_key][0]:
+                indexes[model_date_key] = [offset, f]
+
+    model_best_files = [x[1] for x in list(indexes.values())]
+    
+    target_key = f's3://{bucket}/{key}'
+    if target_key not in model_best_files:
+        print(f'{key} is not a part of the current best time series for its model. Skipping...')
+        return
+
+    model_run_file_count = len(model_best_files)
+    print(f'Aggregating {model_run_file_count} model files for best time series aggregation...')
+
+    # TODO: Generalize this somehow? 
+    mzz = MultiZarrToZarr(
+        model_best_files, 
+        remote_protocol='s3', 
+        remote_options={'anon': True, 'use_ssl': False},
+        concat_dims=['time'],
+        identical_dims=['lon', 'lat', 'lonc', 'latc', 'siglay', 'siglev', 'nele', 'node']
     )
 
     d = mzz.translate()
